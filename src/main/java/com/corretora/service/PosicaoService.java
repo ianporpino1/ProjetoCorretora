c472ed4756966a354e508b5b13574310afe0e63a
ianporpino1 <ianporpino127@gmail.com>
2023-09-18 09:52:25 -0300

    teste

--- src/main/java/com/corretora/service/PosicaoService.java
+++ src/main/java/com/corretora/service/PosicaoService.java
@@ -17,6 +17,7 @@ import java.util.List;
 public class PosicaoService {
     @Autowired
     private PosicaoRepository posicaoRepository;
+    //teste
 
 
     public Posicao findPosicaoByTicker(String ticker){
